f31daf9fca3d4dadec0c7937bbef574300f14277
teedaa <pengtedachengllhs@gmail.com>
2022-10-11 00:20:00 -0700

    Switch from ajax to fetch

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,26 @@
-$(document).ready(function(){
-    $(document).on("submit", ".event-form", function (event){
-        event.preventDefault()
-        var cityName = $("#cityName").val()
-        var APIKey = "f33477ee6cb130320adcdd14a83733fa"
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey
+var city = document.querySelector(".form-control");
+var button = document.querySelector(".btn");
+var currentContainer = document.querySelector("#currentWeather")
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response){
-            console.log(response)
-        })
+$(document).ready(function(){
+    $(document).on("submit", function (event){
+        event.preventDefault();
+
+       fetch("https://api.openweathermap.org/data/2.5/weather?q="+city.value+"&appid=f33477ee6cb130320adcdd14a83733fa")
+       .then(function(response){
+        return response.json();
+       })
+       .then(function (data){
+        console.log(data);
+        for (var i = 0; i < data.length; i++){
+            var temp = document.createElement('p');
+            var wind = document.createElement('p');
+            var humidity = document.createElement('p');
+            temp.textContent = data[i].main.temp;
+            console.log(temp)
+        
+        }
+       })
     })
 
 });
