0477ff36be810e0eb2ac69ab30846b74500784c7
teedaa <pengtedachengllhs@gmail.com>
2022-10-11 02:02:27 -0700

    Link weather icons

--- assets/script.js
+++ assets/script.js
@@ -13,10 +13,13 @@ $(document).ready(function(){
        .then(function (data){
         console.log(data);
             var cityName = data.name;
+            var weatherIcon =data.weather[0].icon
+            var iconURL="https://openweathermap.org/img/wn/"+weatherIcon +"@2x.png"
             var cityEl = document.createElement('h2');
-            cityEl.textContent = (cityName + " (" + today + ")");
+            cityEl.innerHTML =(cityName + " (" + today + ")" + "<img src="+iconURL+">");
 
             var currentTemp = data.main.temp;
+            
             // var convertedTemp = ((currentTemp - 273.15) * 1.80 + 32);
             var tempEl = document.createElement('p')
             tempEl.textContent =("Temp: "+ currentTemp.toFixed(2) + "°F");
