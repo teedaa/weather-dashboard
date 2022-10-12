8e21bef65c78ee65006b39bcd2672cae9e8a4245
teedaa <pengtedachengllhs@gmail.com>
2022-10-12 00:44:37 -0700

    Finalize forecast

--- assets/script.js
+++ assets/script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
             var windSpeed = data.wind.speed;
             // var windEl = document.createElement('p');
-            $(currentWind).html("Wind Speed: " + windSpeed ); 
+            $(currentWind).html("Wind Speed: " + windSpeed + "MPH" ); 
            
 
             var humidity = data.main.humidity;
@@ -54,26 +54,41 @@ $(document).ready(function(){
                .then(function(forecast){
                 console.log(forecast);
 
-            $("#futureDate1").html(moment().add(1, "day").format("l")); 
+            var weatherIcon =forecast.list[0].weather[0].icon
+            var newURL="https://openweathermap.org/img/wn/"+weatherIcon+"@2x.png"
+            $("#futureDate1").html(moment().add(1, "day").format("l") + "<img src="+ newURL+ ">"); 
             $("#futureTemp1").html("Temperature: " +forecast.list[0].main.temp + "°F");
             $("#futureWind1").html("Wind Speed: " + forecast.list[0].wind.speed + "MPH");
             $("#futureHumidity1").html("Humidity: " + forecast.list[0].main.humidity +"%");
-               })
-               
 
-
-            //    for (i=0;i<5;i++){
-            //     var futureDate= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
-            //     var weatherIcon= response.list[((i+1)*8)-1].weather[0].icon;
-            //     var iconURL="https://openweathermap.org/img/wn/"+weatherIcon+".png";
-            //     var futureTemp= response.list[((i+1)*8)-1].main.temp;
-            //     var futureHumidity= response.list[((i+1)*8)-1].main.humidity;
+            var weatherIcon2 =forecast.list[1].weather[0].icon
+            var newURL2="https://openweathermap.org/img/wn/"+weatherIcon2+"@2x.png"
+            $("#futureDate2").html(moment().add(2, "day").format("l") + "<img src="+newURL2+">"); 
+            $("#futureTemp2").html("Temperature: " +forecast.list[1].main.temp + "°F");
+            $("#futureWind2").html("Wind Speed: " + forecast.list[1].wind.speed + "MPH");
+            $("#futureHumidity2").html("Humidity: " + forecast.list[1].main.humidity +"%");
             
-            //     $("#futureDate"+i).html(date);
-            //     $("#futureImg"+i).html("<img src="+iconURL+">");
-            //     $("#futureTemp"+i).html(temp+"°F");
-            //     $("#futureHumidity"+i).html(humidity+"%");
-            // }
+            var weatherIcon3 =forecast.list[2].weather[0].icon
+            var newURL3="https://openweathermap.org/img/wn/"+weatherIcon3+"@2x.png"
+            $("#futureDate3").html(moment().add(3, "day").format("l") + "<img src="+newURL3+">"); 
+            $("#futureTemp3").html("Temperature: " +forecast.list[2].main.temp + "°F");
+            $("#futureWind3").html("Wind Speed: " + forecast.list[2].wind.speed + "MPH");
+            $("#futureHumidity3").html("Humidity: " + forecast.list[2].main.humidity +"%");
+
+            var weatherIcon4 =forecast.list[3].weather[0].icon
+            var newURL4="https://openweathermap.org/img/wn/"+weatherIcon4+"@2x.png"
+            $("#futureDate4").html(moment().add(4, "day").format("l") + "<img src="+newURL4+">"); 
+            $("#futureTemp4").html("Temperature: " +forecast.list[3].main.temp + "°F");
+            $("#futureWind4").html("Wind Speed: " + forecast.list[3].wind.speed + "MPH");
+            $("#futureHumidity4").html("Humidity: " + forecast.list[3].main.humidity +"%");
+
+            var weatherIcon5 =forecast.list[4].weather[0].icon
+            var newURL5="https://openweathermap.org/img/wn/"+weatherIcon5+"@2x.png"
+            $("#futureDate5").html(moment().add(5, "day").format("l") + "<img src="+newURL5+">"); 
+            $("#futureTemp5").html("Temperature: " +forecast.list[4].main.temp + "°F");
+            $("#futureWind5").html("Wind Speed: " + forecast.list[4].wind.speed + "MPH");
+            $("#futureHumidity5").html("Humidity: " + forecast.list[4].main.humidity +"%");
+               })
         }
         )
     })
