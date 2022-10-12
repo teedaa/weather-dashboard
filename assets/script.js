a0d8ede7c1b80bfdc3e45922f9cd21de42dcb93b
teedaa <pengtedachengllhs@gmail.com>
2022-10-11 22:58:49 -0700

    add forecast API

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,17 @@ var city = document.querySelector(".form-control");
 var button = document.querySelector(".btn");
 var currentContainer = document.querySelector("#currentWeather");
 var today = moment().format("l");
+var currentCity = $("#currentCity");
+var currentTemperature = $("#currentTemp");
+var currentWind = $("#currentWind");
+var currentHumidity = $("#currentHumidity");
+
 $(document).ready(function(){
+
     $(document).on("submit", function (event){
         event.preventDefault();
 
-       fetch("https://api.openweathermap.org/data/2.5/weather?q="+city.value+"&&units=imperial&appid=f33477ee6cb130320adcdd14a83733fa")
+       fetch("https://api.openweathermap.org/data/2.5/weather?q="+city.value+"&units=imperial&appid=f33477ee6cb130320adcdd14a83733fa")
        .then(function(response){
         return response.json();
        })
@@ -14,36 +20,45 @@ $(document).ready(function(){
         console.log(data);
             var cityName = data.name;
             var weatherIcon =data.weather[0].icon
-            var iconURL="https://openweathermap.org/img/wn/"+weatherIcon +"@2x.png"
-            var cityEl = document.createElement('h2');
-            cityEl.innerHTML =(cityName + " (" + today + ")" + "<img src="+iconURL+">");
+            var iconURL="https://openweathermap.org/img/wn/"+weatherIcon+"@2x.png"
+            $(currentCity).html(cityName + " (" + today + ")" + "<img src="+iconURL+">");
+        
 
             var currentTemp = data.main.temp;
             
+            
             // var convertedTemp = ((currentTemp - 273.15) * 1.80 + 32);
-            var tempEl = document.createElement('p')
-            tempEl.textContent =("Temp: "+ currentTemp.toFixed(2) + "°F");
+            // var tempEl = document.createElement('p')
+            // tempEl.textContent =
+            $(currentTemperature).html("Temperature: " + currentTemp.toFixed(2) + "°F");
+            
 
             var windSpeed = data.wind.speed;
-            var windEl = document.createElement('p');
-            windEl.textContent =(windSpeed); 
+            // var windEl = document.createElement('p');
+            $(currentWind).html("Wind Speed: " + windSpeed ); 
+           
 
             var humidity = data.main.humidity;
-            var humidityEl = document.createElement('p');
-            humidityEl.textContent =("Humidity: " + humidity + "%");
+            // var humidityEl = document.createElement('p');
+            $(currentHumidity).html("Humidity: " + humidity + "%");
 
-
-            currentContainer.append(cityEl, tempEl, windSpeed, humidityEl);
-
-        }
-       )
-    })
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+city.value+"&cnt=5&units=imperial&appid=f33477ee6cb130320adcdd14a83733fa")
-       .then(function(response){
-        return response.json();
-       })
-       .then(function(forecast){
-        console.log(forecast);
             
-       })
+            // currentContainer.append(cityEl, tempEl, windSpeed, humidityEl);
+            var lat = data.coord.lat
+            var lon = data.coord.lon
+
+            fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&units=imperial&appid=f33477ee6cb130320adcdd14a83733fa")
+               .then(function(response){
+                return response.json();
+               })
+               .then(function(forecast){
+                console.log(forecast);
+                    
+               })
+               
+        }
+        )
+    })
+
 });
+// `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`;
