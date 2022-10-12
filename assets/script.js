e2014d46dd37d68dd0b858a0f9b5461f5a2b5446
teedaa <pengtedachengllhs@gmail.com>
2022-10-11 22:56:07 -0700

    Add fetch for forecast API

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,9 @@ var currentCity = $("#currentCity");
 var currentTemperature = $("#currentTemp");
 var currentWind = $("#currentWind");
 var currentHumidity = $("#currentHumidity");
+
 $(document).ready(function(){
+
     $(document).on("submit", function (event){
         event.preventDefault();
 
@@ -20,34 +22,43 @@ $(document).ready(function(){
             var weatherIcon =data.weather[0].icon
             var iconURL="https://openweathermap.org/img/wn/"+weatherIcon+"@2x.png"
             $(currentCity).html(cityName + " (" + today + ")" + "<img src="+iconURL+">");
+        
 
             var currentTemp = data.main.temp;
+            
             
             // var convertedTemp = ((currentTemp - 273.15) * 1.80 + 32);
             // var tempEl = document.createElement('p')
             // tempEl.textContent =
-            $(currentTemperature).html(currentTemp.toFixed(2) + "°F");
+            $(currentTemperature).html("Temperature: " + currentTemp.toFixed(2) + "°F");
+            
 
             var windSpeed = data.wind.speed;
             // var windEl = document.createElement('p');
-            $(currentWind).html(windSpeed + "MPH"); 
+            $(currentWind).html("Wind Speed: " + windSpeed ); 
+           
 
             var humidity = data.main.humidity;
             // var humidityEl = document.createElement('p');
-            $(currentHumidity).html(humidity + "%");
+            $(currentHumidity).html("Humidity: " + humidity + "%");
 
-
-            // currentContainer.append(cityEl, tempEl, windSpeed, humidityEl);
-
-        }
-       )
-    })
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+city.value+"&units=imperial&appid=f33477ee6cb130320adcdd14a83733fa")
-       .then(function(response){
-        return response.json();
-       })
-       .then(function(forecast){
-        console.log(forecast);
             
-       })
+            // currentContainer.append(cityEl, tempEl, windSpeed, humidityEl);
+            var lat = data.coord.lat
+            var lon = data.coord.lon
+
+            fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&units=imperial&appid=f33477ee6cb130320adcdd14a83733fa")
+               .then(function(response){
+                return response.json();
+               })
+               .then(function(forecast){
+                console.log(forecast);
+                    
+               })
+               
+        }
+        )
+    })
+
 });
+// `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`;
